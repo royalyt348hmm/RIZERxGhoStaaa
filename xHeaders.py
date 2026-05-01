2af72a6046967d54a2257ea81194876ce86ca705
royalyt348hmm <royalyt348@gmail.com>
2026-05-01 10:59:09 +0600

    Update API endpoint for version retrieval

--- xHeaders.py
+++ xHeaders.py
@@ -81,7 +81,7 @@ def AuToUpDaTE():
     version = result['version']
 
 
-    r = requests.get(f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993').json()
+    r = requests.get(f'https://version.ggwhitehawk.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993').json()
     #print(r['server_url'],r['latest_release_version'])
     url , ob = r['server_url'] , r['latest_release_version']
     
